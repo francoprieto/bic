4dab5783401d39995805b376308016701ef1cf11
Franco Prieto <francoprieto@gmail.com>
2023-06-25 19:32:34 -0400

    Iniciando programacion del firmador

--- src/main/java/py/org/firmador/birome/FirmadorImpl.java
+++ src/main/java/py/org/firmador/birome/FirmadorImpl.java
@@ -1,10 +1,27 @@
 package py.org.firmador.birome;
 
+import py.org.firmador.Log;
+import py.org.firmador.dto.Conf;
+import py.org.firmador.exceptions.UnsupportedPlatformException;
+import py.org.firmador.util.ConfiguracionUtil;
+
 import java.util.Map;
 
 public class FirmadorImpl implements Firmador{
     public String firmar(Map<String,String> parametros){
-        
+        Conf configuracion = null;
+        try {
+            if (parametros.containsKey("init") && parametros.get("init").equals("true"))
+                configuracion = ConfiguracionUtil.init(true);
+            else
+                configuracion = ConfiguracionUtil.init();
+        }catch(UnsupportedPlatformException exception){
+            Log.error("Plataforma no soportada");
+            return "error";
+        }
+
+
+
         return null;
     }
 }
