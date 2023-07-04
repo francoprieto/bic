7aadc936d33cc3d6cc73f6b6eb8f3f30d606aceb
Franco Prieto <francoprieto@gmail.com>
2023-07-04 17:17:01 -0400

    Trabajo en progreso

--- src/main/java/py/org/firmador/util/WebUtil.java
+++ src/main/java/py/org/firmador/util/WebUtil.java
@@ -3,6 +3,14 @@ package py.org.firmador.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.HttpMultipartMode;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
 import py.org.firmador.Log;
 
 import java.io.File;
@@ -16,6 +24,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WebUtil {
+
+    public static void upload(File file, String uri, Map<String,String> headers, Map<String,String> parameters){
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.setMode(HttpMultipartMode.LEGACY);
+        for(Map.Entry<String,String> entry : parameters.entrySet()){
+            if(entry.getValue().equals("{data}"))
+                builder.addBinaryBody(entry.getKey(), file, ContentType.DEFAULT_BINARY, file.getName());
+            else
+                builder.addTextBody(entry.getKey(),entry.getValue());
+        }
+        HttpEntity entity = builder.build();
+        HttpPost post = new HttpPost(uri);
+
+        for(Map.Entry<String,String> entry : headers.entrySet())
+            post.addHeader(entry.getKey(), entry.getValue());
+
+        post.setEntity(entity);
+
+        try(CloseableHttpClient client = HttpClientBuilder.create().build()){
+
+            CloseableHttpResponse response = (CloseableHttpResponse) client.execute(post);
+            if(response.getCode() == )
+            HttpEntity respuesta = response.getEntity();
+
+        }catch(IOException ex){
+
+        }
+
+    }
 
     public static boolean descargar(String origen, String destino, Long dto, Long rto) throws IOException {
         URL url = getArchivoURL(origen);
