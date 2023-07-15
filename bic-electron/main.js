2cb71f4e38a42df84d7969c8896b6476f707610e
Franco Prieto <francoprieto@gmail.com>
2023-07-14 22:04:35 -0400

    Falta un endpoint

--- bic-electron/main.js
+++ bic-electron/main.js
@@ -8,11 +8,11 @@
  * bic://
  */
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 // Module with utilities for working with file and directory paths.
-const path = require('path')
+const path = require('path');
 // Module with utilities for URL resolution and parsing.
-const url = require('url')
+const url = require('url');
 
 
 const bicurl = 'http://localhost:4200/bic';
@@ -25,7 +25,8 @@ let mainWindow
 let deeplinkingUrl = '';
 
 // Force Single Instance Application
-const gotTheLock = app.requestSingleInstanceLock()
+const gotTheLock = app.requestSingleInstanceLock();
+
 if (gotTheLock) {
 	app.on('second-instance', (e, argv) => {
 		// Someone tried to run a second instance, we should focus our window.
@@ -34,23 +35,22 @@ if (gotTheLock) {
 		// argv: An array of the second instance’s (command line / deep linked) arguments
 		if (process.platform == 'win32') {
 			// Keep only command line / deep linked arguments
-			deeplinkingUrl = argv.slice(1)
+			deeplinkingUrl = argv.slice(1);
 		}
-		logEverywhere('app.makeSingleInstance# ' + deeplinkingUrl)
+		logEverywhere('app.makeSingleInstance2# ' + deeplinkingUrl);
 
 		if (mainWindow) {
 			if (mainWindow.isMinimized()) mainWindow.restore()
-			mainWindow.focus()
+			mainWindow.focus();
 		}
-	})
+	});
 } else {
-	app.quit()
-	return
+	app.quit();
+	return;
 }
 
-function firmar(archivo, pin){
-
-}
+//function firmar(archivo, pin){
+//}
 
 function createWindow() {
 	// Create the browser window.
@@ -60,73 +60,73 @@ function createWindow() {
 		webPreferences: {
 			nodeIntegration: true
 		}
-	})
+	});
 
 	// and load the index.html of the app.
-	//mainWindow.loadURL(bicurl);
+	//mainWindow.loadURL(''bicurl'');
 
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 	// Protocol handler for win32
 	if (process.platform == 'win32') {
 		// Keep only command line / deep linked arguments
-		deeplinkingUrl = process.argv.slice(1)
+		deeplinkingUrl = process.argv.slice(1);
 	}
-
+    logEverywhere('createWindow1# ' + deeplinkingUrl);
 	if (deeplinkingUrl && deeplinkingUrl.length > 1) {
-		let params = deeplinkingUrl.replaceAll('bic://', '');
-        logEverywhere('createWindow# ' + params);
+        logEverywhere('createWindow# ' + deeplinkingUrl);
 	}
 
-    mainWindow.loadURL(__dirname + '/index.html');
+    mainWindow.loadURL('https://blank.page/');
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null
-	})
+		mainWindow = null;
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
-})
+});
 
 app.on('activate', function () {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) {
-		createWindow()
+		createWindow();
 	}
-})
+});
 
 if (!app.isDefaultProtocolClient('bic')) {
 	// Define custom protocol handler. Deep linking works on packaged versions of the application!
-	app.setAsDefaultProtocolClient('bic')
+	app.setAsDefaultProtocolClient('bic');
 }
 
 app.on('will-finish-launching', function () {
 	// Protocol handler for osx
 	app.on('open-url', function (event, url) {
-		event.preventDefault()
+		event.preventDefault();
+		logEverywhere('OSX loading..' + url);
 		deeplinkingUrl = url;
-	})
-})
+	});
+});
 
 // Log both at dev console and at running node console instance
 function logEverywhere(s) {
-	console.log(s)
+	console.log(s);
 	if (mainWindow && mainWindow.webContents) {
-		mainWindow.webContents.executeJavaScript(`console.log("${s}")`)
+		mainWindow.webContents.executeJavaScript(`console.log("${s}")`);
 	}
 }
